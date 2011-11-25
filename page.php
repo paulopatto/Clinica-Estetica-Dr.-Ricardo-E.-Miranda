2a34467372beab66b47ee179e442ca3b65101667
Paulo Patto <paulopatto@gmail.com>
2011-11-25 04:04:55 -0200

    finalizado a parte estrutural

--- page.php
+++ page.php
@@ -13,7 +13,7 @@
 		<link rel="alternate" type="application/rss+xml" 	title="RSS 2.0" 	href="<?php bloginfo('rss2_url'); ?>" />
 		<link rel="alternate" type="text/xml" 				title="RSS .92" 	href="<?php bloginfo('rss_url'); ?>" />
 		<link rel="alternate" type="application/atom+xml" 	title="Atom 0.3" 	href="<?php bloginfo('atom_url'); ?>" />
-		
+		<?php wp_enqueue_script("jquery"); ?>
 		<?php
 			/// Folhas de estilos
 			css_include('nivo-slider');
@@ -22,14 +22,6 @@
 			wp_head();
 		?>
 		
-		<?php
-			/// Scripts javascript
-			wp_enqueue_script("jquery");
-			
-			js_include($filename='jquery.corner');
-			js_include($filename='jquery.nivo.slider');
-			js_include($filename='application');
-		?>
 	</head>
 	<body>
 		<div id='header'>
@@ -40,9 +32,14 @@
 			<div id='page-content'>
 				<?php the_post(); ?>
 				<?php the_content(); ?>
+				<br />
+				<img src='<?php bloginfo("template_url"); ?>/images/titulo_comentarios.jpg' />
+				<?php comments_template( '', true ); ?>
 			</div>
+			<!--
+				Se for a página de cirurgias e/ou estética, exibe o menu lateral sidebar
+			-->
 			<?php if(!(get_the_title() != "Cirurgias" && get_the_title() != "Estética")){ ?>
-			
 			<div id='sidebar-page'>
 				<strong style='color:#00465F;'><?php the_title(); ?></strong><br />
 				<hr />
@@ -62,6 +59,7 @@
 			</div>
 			
 			<?php } // end if |the_title() == "Cirurgias" || the_title() == "Estética"| ?>
+			<!--Fim do sidebar-->
 		</div>
 		
 		<div id='footer'>
@@ -69,4 +67,9 @@
 		</div>
 		
 	</body>
+	<?php
+		js_include($filename='jquery.corner');
+		js_include($filename='jquery.nivo.slider');
+		js_include($filename='application');
+	?>
 </html>
